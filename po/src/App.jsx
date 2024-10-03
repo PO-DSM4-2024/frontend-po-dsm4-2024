73836aff0a4820b29750a5156f9f926ac7110898
gilSFranco <gilmarsoaresfranco@gmail.com>
2024-10-02 22:13:19 -0300

    fix: using prittier as default formatter

--- po/src/App.jsx
+++ po/src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Query from "./pages/Query/Query";
-import TeacherLogin from "./pages/TeacherHome/TeacherLogin";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Query from './pages/Query/Query';
+import TeacherLogin from './pages/TeacherHome/TeacherLogin';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/teacherLogin' element={<TeacherLogin/>}/>
+        <Route path="/teacherLogin" element={<TeacherLogin />} />
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/query" element={<Query />} />
@@ -17,4 +17,4 @@ const App = () => {
   );
 };
 
-export default App
+export default App;
